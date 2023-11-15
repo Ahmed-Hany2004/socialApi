c0e84fbb15b4076ebfbb9d9b74a57a152744ce71
Ahmed <ahmedhanycc053@gmail.com>
2023-11-16 01:07:48 +0200

    first updite cors2

--- validationSchema/userSchema.js
+++ validationSchema/userSchema.js
@@ -4,7 +4,7 @@ const joi =require("joi");
 const userschema = joi.object({
    username:joi.string().trim().min(3).max(20).required(),
    password:joi.string().trim().min(3).max(20).required(),
-   email:joi.string().trim().min(3).max(20),
+   email:joi.string().trim().min(3),
    
 })
 
