4100606f20d4329f88e572e6a8138a0f01969725
Ahmed <ahmedhanycc053@gmail.com>
2023-11-16 22:37:22 +0200

    first updite cor5

--- validationSchema/userSchema.js
+++ validationSchema/userSchema.js
@@ -11,8 +11,8 @@ const userschema = joi.object({
 
 const userupdateschema  = joi.object({
    username:joi.string().trim().min(3).max(20).required(),
-   bio:joi.string().trim().min(3).max(200),
-   email:joi.string().trim().min(3),
+   bio:joi.trim().min(3).max(200),
+   email:joi.trim().min(3),
 })
 
 
